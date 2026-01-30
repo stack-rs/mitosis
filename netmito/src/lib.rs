ec96563e742b1b5f6e47816b1e9addbfcefe63a5
BobAnkh <bobankhshen@gmail.com>
2026-01-30 09:13:00 +0000

    tmp

--- netmito/src/lib.rs
+++ netmito/src/lib.rs
@@ -1,15 +1,16 @@
+pub mod agent;
 pub mod api;
 pub mod client;
 pub mod config;
 pub mod coordinator;
 pub mod entity;
 pub mod error;
-pub mod manager;
 pub mod migration;
 pub mod schema;
 pub mod service;
 pub mod signal;
 pub mod worker;
+pub mod ws;
 pub mod reexports {
     pub use redis;
     pub use time;

e2106428c1c17508ae0324d06e733e55dd38bdc2
un-lock-able <gaogao174561@outlook.com>
2026-07-15 15:13:45 +0800

    refactor(db): remove wildcard match in task suite migration

--- netmito/src/migration/m20260703_000001_alter_tasks_link_suites.rs
+++ netmito/src/migration/m20260703_000001_alter_tasks_link_suites.rs
@@ -11,7 +11,18 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        for tbl in [Tasks::ActiveTasks, Tasks::ArchivedTasks] {
+        for (tbl, fk, idx) in [
+            (
+                Tasks::ActiveTasks,
+                "fk-active_tasks-task_suite_id",
+                "idx_active_tasks-task_suite_id",
+            ),
+            (
+                Tasks::ArchivedTasks,
+                "fk-archived_tasks-task_suite_id",
+                "idx_archived_tasks-task_suite_id",
+            ),
+        ] {
             manager
                 .alter_table(
                     Table::alter()
@@ -26,10 +37,7 @@ impl MigrationTrait for Migration {
                         .table(tbl)
                         .add_foreign_key(
                             TableForeignKey::new()
-                                .name(match tbl {
-                                    Tasks::ActiveTasks => "fk-active_tasks-task_suite_id",
-                                    _ => "fk-archived_tasks-task_suite_id",
-                                })
+                                .name(fk)
                                 .from_tbl(tbl)
                                 .from_col(Tasks::TaskSuiteId)
                                 .to_tbl(TaskSuites::Table)
@@ -44,10 +52,7 @@ impl MigrationTrait for Migration {
                 .create_index(
                     Index::create()
                         .if_not_exists()
-                        .name(match tbl {
-                            Tasks::ActiveTasks => "idx_active_tasks-task_suite_id",
-                            _ => "idx_archived_tasks-task_suite_id",
-                        })
+                        .name(idx)
                         .table(tbl)
                         .col(Tasks::TaskSuiteId)
                         .and_where(Expr::col(Tasks::TaskSuiteId).is_not_null())
